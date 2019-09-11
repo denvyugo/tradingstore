a054f2e98d21a9959be9a612874b9cab20122de1
denvyugo <denvyugo@gmail.com>
2019-09-11 23:55:04 +0300

    initial commit

--- settings/test_get_settings.py
+++
@@ -1,9 +0,0 @@
-def get_token():
-    with open('token.txt', 'r') as file:
-        token, bot_name = file.readline().split(sep=';')
-    return token.split(sep=' = ')[1]
-
-
-if __name__ == '__main__':
-    token = get_token()
-    print(token)
