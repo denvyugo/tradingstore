d2c710f4cac91da0a6a626decc8958cfd382d8d3
volhen2485 <zver2485@mail.ru>
2019-10-01 12:11:21 +0300

    Test WebHook

--- webhook_telbot.py
+++ webhook_telbot.py
@@ -1,17 +1,17 @@
-import logging
+import lologging
 import cherrypy
 import telebot
 from settings import config
 from handlers.handler_main import HandlerMain
 
-API_TOKEN = '<api_token>'
+API_TOKEN = config.TOKEN
 
-WEBHOOK_HOST = '<ip/host where the bot is running>'
+WEBHOOK_HOST = '195.161.62.228'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = 'webhooks/webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = 'webhooks/webhook_pkey.pem'  # Path to the ssl private key
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
@@ -21,29 +21,9 @@ telebot.logger.setLevel(logging.INFO)
 
 bot = telebot.TeleBot(API_TOKEN)
 
-class TelBot:
-    """
-    Основной класс телеграмм бота(Сервер), в основе которого 
-    используется библиотека pyTelegramBotAPI
-    """
-    __version__ = config.VERSION
-    __author__ = config.AUTHOR
 
-    def __init__(self):
-        """
-        Инициализация бота(Сервера)
-        """
-	logger = telebot.logger
-
-	telebot.logger.setLevel(logging.INFO)
-        # Получение токена
-        self.token = config.TOKEN
-        # Инициализация бота на основе зарегистрированного токена
-        self.bot = telebot.TeleBot(self.token)
-        # Инициализация оброботчика событий
-        self.handler = HandlerMain(self.bot)
-    
-    class WebhookServer(object):
+# WebhookServer, process webhook calls
+class WebhookServer(object):
     @cherrypy.expose
     def index(self):
         if 'content-length' in cherrypy.request.headers and \
@@ -57,11 +37,27 @@ class TelBot:
         else:
             raise cherrypy.HTTPError(403)
 
-    bot.remove_webhook()
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
+
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
 
 # Set webhook
 bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-	        certificate=open(WEBHOOK_SSL_CERT, 'r'))
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Disable CherryPy requests log
 access_log = cherrypy.log.access_log
@@ -70,12 +66,11 @@ for handler in tuple(access_log.handlers):
 
 # Start cherrypy server
 cherrypy.config.update({
-    'server.socket_host'    : WEBHOOK_LISTEN,
-    'server.socket_port'    : WEBHOOK_PORT,
-    'server.ssl_module'     : 'builtin',
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
     'server.ssl_certificate': WEBHOOK_SSL_CERT,
     'server.ssl_private_key': WEBHOOK_SSL_PRIV
 })
 
 cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
-
