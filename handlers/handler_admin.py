d9e801bfc11b28821d9fe889e2bc49aac69047a3
denvyugo <42419392+denvyugo@users.noreply.github.com>
2019-10-24 17:26:12 +0300

    administrator - dialog for company information

--- handlers/handler_admin.py
+++ handlers/handler_admin.py
@@ -1,7 +1,7 @@
 import json
 import shelve
 # импортируем настройки и утилиты
-from settings import config
+from settings.config import ADMINISTRATIVE, DialogState
 # импортируем ответ пользователю
 from settings.message import MESSAGES
 # импортируем класс родитель
@@ -14,7 +14,7 @@ class HandlerAdmin(Handler):
         super().__init__(bot)
 
     def handle(self):
-        @self.bot.message_handler(func=lambda message: message.text == config.ADMINISTRATIVE['PROPERTY'])
+        @self.bot.message_handler(func=lambda message: message.text == ADMINISTRATIVE['PROPERTY'])
         def check_property(message):
             """
             check if company info json file exists
@@ -35,7 +35,7 @@ class HandlerAdmin(Handler):
             else:
                 reply = MESSAGES['no_company']
                 markup = self.keybords.company_add()
-            admin.dialog_status=config.DialogState.NoDialog
+            admin.dialog_status = DialogState.NoDialog
             self.bot.send_message(message.chat.id, reply, parse_mode='HTML', reply_markup=markup)
 
         @self.bot.message_handler(func=lambda message: _start_dialog_property(message.text))
@@ -51,10 +51,10 @@ class HandlerAdmin(Handler):
                 reply = '<b>Компания:</b> {}<br>Введите другое название, чтобы изменить:'.format(company)
             else:
                 reply = 'Введите название Вашей компании:'
-            admin.dialog_status=config.DialogState.CompanyName
+            admin.dialog_status = DialogState.CompanyName
             self.bot.send_message(message.chat.id, reply, parse_mode='HTML')
 
-        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == config.DialogState.CompanyName)
+        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == DialogState.CompanyName)
         def input_company_name(message):
             """
             input company name
@@ -64,35 +64,79 @@ class HandlerAdmin(Handler):
             admin = Admin(chat_id=message.chat.id)
             if admin.set_company_name(message.text):
                 reply = 'Компания: {}'.format(message.text)
-                admin.dialog_status = config.DialogState.CompanyTaxpayer
+                admin.dialog_status = DialogState.CompanyTaxpayer
             else:
                 reply = 'Повторите, пожалуйста ввод'
             self.bot.send_message(message.chat.id, reply)
 
-        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == \
-                                                       config.DialogState.CompanyTaxpayer)
-        def input_company_name(message):
+        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == DialogState.CompanyTaxpayer)
+        def input_company_taxpayer(message):
             """
-            input company name
+            input company taxpayer ID
             :param message:
             :return:
             """
             admin = Admin(chat_id=message.chat.id)
-            if admin.set_company_name(message.text):
-                reply = 'Компания: {}'.format(message.text)
-                admin.dialog_status = config.DialogState.CompanyTaxpayer
+            if admin.set_company_taxpayer(message.text):
+                reply = 'ИНН: {}'.format(message.text)
+                admin.dialog_status = DialogState.CompanyRegistrationID
             else:
                 reply = 'Повторите, пожалуйста ввод'
             self.bot.send_message(message.chat.id, reply)
 
+        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == DialogState.CompanyRegistrationID)
+        def input_company_registration(message):
+            """
+            input company registration ID
+            :param message:
+            :return:
+            """
+            admin = Admin(chat_id=message.chat.id)
+            if admin.set_company_registry(message.text):
+                reply = 'КПП: {}'.format(message.text)
+                admin.dialog_status = DialogState.CompanyAddress
+            else:
+                reply = 'Повторите, пожалуйста ввод'
+            self.bot.send_message(message.chat.id, reply)
+
+        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == DialogState.CompanyAddress)
+        def input_company_address(message):
+            """
+            input company address
+            :param message:
+            :return:
+            """
+            admin = Admin(chat_id=message.chat.id)
+            if admin.set_company_address(message.text):
+                reply = 'Адрес: {}'.format(message.text)
+                admin.dialog_status = DialogState.CompanyPhone
+            else:
+                reply = 'Повторите, пожалуйста ввод'
+            self.bot.send_message(message.chat.id, reply)
+
+        @self.bot.message_handler(func=lambda message: _dialog_state(message.chat.id) == DialogState.CompanyRegistrationID)
+        def input_company_phone(message):
+            """
+            input company phone
+            :param message:
+            :return:
+            """
+            admin = Admin(chat_id=message.chat.id)
+            if admin.set_company_phone(message.text):
+                reply = 'Телефон: {}'.format(message.text)
+                admin.dialog_status = DialogState.CompanyEmail
+            else:
+                reply = 'Повторите, пожалуйста ввод'
+            self.bot.send_message(message.chat.id, reply)
+        
         def _start_dialog_property(message):
             """
             if administrator tap PROPERTY CHANGE or PROPERTY ADD button, then start dialog
             :param message:
             :return True: if change or add property, False: otherwise
             """
-            return message == config.ADMINISTRATIVE['PROPERTY_CHANGE'] or \
-                   message == config.ADMINISTRATIVE['PROPERTY_ADD']
+            return message == ADMINISTRATIVE['PROPERTY_CHANGE'] or \
+                   message == ADMINISTRATIVE['PROPERTY_ADD']
 
         def _dialog_state(chat_id):
             """
